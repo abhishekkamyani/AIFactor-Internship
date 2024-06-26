5fd48e3a64f86fbac821b4411b111fefa97d392a
abhishekraj3712 <rockstarabhi805@gmail.com>
2024-06-26 11:28:05 +0500

    Completed

--- React Flow Logic Gates/src/components/Sidebar.jsx
+++ React Flow Logic Gates/src/components/Sidebar.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+const nodesData = [
+  { src: "./images/AND.png", type: "and" },
+  { src: "./images/OR.png", type: "or" },
+  { src: "./images/NOT.png", type: "not" },
+  { src: "./images/Switch-Off.png", type: "switchButton", additionalClasses: "border border-black" },
+  { src: "./images/Light-Off.png", type: "light" },
+]
 
 export default function Sidebar() {
   const onDragStart = (e, nodeType) => {
@@ -7,20 +13,6 @@ export default function Sidebar() {
 
     e.dataTransfer.effectAllowed = "move";
   };
-
-
-  const onTouchStart = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    // alert("Touch start worked");
-  };
-
-  const onTouchMove = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    alert("Touch move worked");
-  };
-
 
   return (
     <div className="flex basis-1/5 h-screen bg-black select-none">
@@ -34,21 +26,10 @@ export default function Sidebar() {
         {/* items */}
         <nav className="bg-gray-200 basis-full pt-5 overflow-y-auto">
           <ul className="flex flex-col items-center gap-y-5">
-            <li className="sidebar-item">
-              <img src="./images/AND.png" className="sidebar-item-img" onTouchStart={onTouchStart} />
+            {nodesData.map((node, index) => <li className="sidebar-item" key={index}>
+              <img src={node.src} className={`sidebar-item-img ${node.additionalClasses}`} onDragStart={(e) => onDragStart(e, node.type)} />
             </li>
-            <li className="sidebar-item">
-              <img src="./images/OR.png" className="sidebar-item-img" onDragStart={(e) => onDragStart(e, "or")} onTouchMove={onTouchMove} />
-            </li>
-            <li className="sidebar-item">
-              <img src="./images/NOT.png" className="sidebar-item-img" onDragStart={(e) => onDragStart(e, "not")} />
-            </li>
-            <li className="sidebar-item">
-              <img src="./images/Switch-Off.png" className="sidebar-item-img border border-black" onDragStart={(e) => onDragStart(e, "switchButton")} />
-            </li>
-            <li className="sidebar-item border-none">
-              <img src="./images/Light-Off.png" className="sidebar-item-img rotate-90" onDragStart={(e) => onDragStart(e, "light")} />
-            </li>
+            )}
           </ul>
         </nav>
       </div>
