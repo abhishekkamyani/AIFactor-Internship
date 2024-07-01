f956517d427735c01910cdb1f582bb5c4e8c3996
abhishekraj3712 <rockstarabhi805@gmail.com>
2024-07-01 14:54:23 +0500

    hj

--- Apache Echarts React/src/App.jsx
+++ Apache Echarts React/src/App.jsx
@@ -4,7 +4,7 @@ import MyChart from './components/MyChart'
 export default function App() {
   return (
     <div>
-      <div className='bg-green-400 w-1/2 lg:w-full'>
+      <div className='w-1/2 lg:w-full'>
       <MyChart />
       </div>
     </div>
--- Apache Echarts React/src/components/MyChart.jsx
+++ Apache Echarts React/src/components/MyChart.jsx
@@ -1,27 +1,89 @@
 import ReactECharts from 'echarts-for-react'; // Import the wrapper
-import * as echarts from 'echarts'; // Import ECharts itself
+// import * as echarts from 'echarts'; // Import ECharts itself
 const option = {
-    title: {
-      text: 'Basic Bar Chart'
-    },
-    tooltip: {},
-    xAxis: {
-      data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
-    },
-    yAxis: {},
-    series: [
-      {
-        name: 'sales',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+  title: {
+    text: 'Basic Bar Chart'
+  },
+  legend: { show: true },
+  tooltip: {},
+  xAxis: {
+    data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'sales',
+      // type: 'scatter',
+      type: 'bar',
+      data: [5, 20, 36, 10, {
+        value: 30,
+        itemStyle: {
+          color: "green",
+          opacity: 0.5,
+        }
+      }, 20],
+      itemStyle: {
+        barBorderRadius: 5,
+        borderWidth: 3,
+        borderType: "dashed",
+        borderColor: "red",
+        shadowColor: "white",
+        shadowBlur: 6,
+      },
+      // barGap: '200%',
+      // barCategoryGap: '40%'
+      // barWidth: "50%"
+      showBackground: true,
+      backgroundStyle: {
+        color: 'rgba(0,0,0, 0.5)'
+      },
+      label:{
+        show: true,
+        // position: 'top',
+        textStyle: {
+          fontSize: 20
+        }
       }
-    ]
-  };
+    },
+    // {
+    //   type: "line",
+    //   name: "line",
+    //   data: [5, 20, 36, 10, 30, 20]
+    // }
+  ]
+};
+
+const optionStackedBar = {
+  title: {
+    text: 'Basic Bar Chart'
+  },
+  tooltip: {},
+  legend: { data: ["2020", "2021"] },
+  xAxis: {
+    data: ['Lenovo', 'Dell', 'Android', 'Samsung']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '2020',
+      type: 'bar',
+      data: [50, 80, 160, 120],
+      stack: "total"
+    },
+    {
+      name: '2021',
+      type: 'bar',
+      data: [70, 60, 300, 190],
+      stack: "total"
+    },
+  ]
+};
+
 export default function MyChart() {
   return (
     <ReactECharts
-    option={option}
-    style={{ height: 400 }}
-  />
+      option={option}
+      style={{ height: 400 }}
+    />
   )
 }
