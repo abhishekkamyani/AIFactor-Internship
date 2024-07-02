6578cf9d153ac6e2f06fe075c1058b5236f4cca2
abhishek kamyani <abhishekkumar.bscsf21kdk@iba-suk.edu.pk>
2024-07-02 21:56:56 +0500

    Completed tanstack query

--- Apache Echarts React/src/App.jsx
+++ Apache Echarts React/src/App.jsx
@@ -4,21 +4,28 @@ import { useQuery } from "@tanstack/react-query";
 import LineChart from "./components/charts/LineChart";
 
 export default function App() {
-  const fetchWeather = async () => {
+  const [selectedCountryName, setSelectedCountryName] = useState(null);
+  const [selectedCityName, setSelectedCityName] = useState(null);
+  console.log(selectedCityName);
+
+  const fetchWeather = async (city) => {
     try {
       const response = await axios.get(
-        "https://api.openweathermap.org/data/2.5/forecast?q=karachi&appid=f4a4c80b35c7d0638a2563b560f4b5e6&units=metric"
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f4a4c80b35c7d0638a2563b560f4b5e6&units=metric`
       );
       // console.log(response.data);
 
       if (response.data && response.data.list) {
         const currentTimestamp = Math.floor(Date.now() / 1000); // Current time in UNIX timestamp
-        const twentyFourHoursLaterTimestamp = currentTimestamp + (24 * 60 * 60); // Timestamp for 6 hours later
+        const twentyFourHoursLaterTimestamp = currentTimestamp + 24 * 60 * 60; // Timestamp for 6 hours later
         // Filter data for the next 6 hours
 
-        const filteredData = response.data.list.filter(item => {
+        const filteredData = response.data.list.filter((item) => {
           const timestamp = item.dt;
-          return timestamp >= currentTimestamp && timestamp <= twentyFourHoursLaterTimestamp;
+          return (
+            timestamp >= currentTimestamp &&
+            timestamp <= twentyFourHoursLaterTimestamp
+          );
         });
 
         response.data.list = filteredData;
@@ -31,22 +38,35 @@ export default function App() {
     }
   };
 
-
   const fetchCountries = async () => {
     try {
-      const response = await axios.get("https://countriesnow.space/api/v0.1/countries/iso");
-      console.log(response.data?.data?.map(country => country.name));
-      return response;
+      const response = await axios.get(
+        "https://countriesnow.space/api/v0.1/countries/iso"
+      );
+      // console.log(response.data?.data?.map((country) => country.name));
+      return response.data?.data?.map((country) => country.name);
     } catch (error) {
       console.log(error);
       throw error;
     }
-  }
+  };
 
+  const fetchCities = async (countryName) => {
+    try {
+      const response = await axios.post(
+        "https://countriesnow.space/api/v0.1/countries/cities",
+        { country: countryName }
+      );
+      return response?.data?.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
 
-  const { data: weather, error } = useQuery({
-    queryKey: "weather",
-    queryFn: fetchWeather,
+  const weather= useQuery({
+    queryKey: ["weather-", selectedCountryName, selectedCityName],
+    queryFn: () => selectedCountryName && selectedCityName && fetchWeather(selectedCityName),
     // refetchInterval: 60 * 60 * 1000, // 1 hour
     // staleTime: 10 * 60 * 1000, // 10 minutes
     // cacheTime: 1 * 60 * 60 * 1000, // 1 hour
@@ -59,30 +79,76 @@ export default function App() {
     refetchOnWindowBlur: false, // Disable automatic refetch on window blur
     // refetchOnMount: false, // Disable automatic refetch on mount
     // refetchOnReconnect
+    staleTime: Infinity, // Disable automatic refetching for cached data
+    cacheTime: Infinity, // Keep cached data forever
   });
 
   const countries = useQuery({
-    queryKey: "countries",
+    queryKey: ["countries"],
     queryFn: fetchCountries,
-    refetchOnWindowFocus: false, 
+    refetchOnWindowFocus: false,
     refetchOnWindowBlur: false,
-  })
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  });
 
-  // console.log(countries);
+  const cities = useQuery({
+    queryKey: ["cities-", selectedCountryName],
+    queryFn: () => selectedCountryName && fetchCities(selectedCountryName),
+    refetchOnWindowFocus: false,
+    refetchOnWindowBlur: false,
+    staleTime: Infinity,
+    cacheTime: Infinity, 
+  });
+
 
   return (
     <div className="py-5">
-      <h1 className="text-center font-extrabold text-3xl">Weather Visualization App</h1>
-      <div className="flex justify-center space-x-20 my-10">
-        <select className="bg-gray-200 p-3 !h-1/2 rounded-lg" name="country" id="country" >
-          <option className="">[-Select Country-]</option>
+      <h1 className="text-center font-extrabold text-3xl">
+        Weather Visualization App
+      </h1>
+      <div className="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-20 my-10">
+        <select
+          className="bg-gray-200 p-3 rounded-lg w-52"
+          name="country"
+          id="country"
+          value={selectedCountryName}
+          onChange={(e) => {
+            setSelectedCountryName(e.target.value)
+            setSelectedCityName(null);
+          }}
+        >
+          <option value={""}>[-Select Country-]</option>
+          {countries?.data?.map((country) => (
+            <option value={country} key={country}>
+              {country}
+            </option>
+          ))}
         </select>
-        <select className="bg-gray-200 p-3 !h-1/2 rounded-lg" name="city" id="city" >
-          <option className="">[-Select City-]</option>
+        <select
+          className="bg-gray-200 p-3 rounded-lg w-52"
+          name="city"
+          id="city"
+          value={selectedCityName}
+          onChange={(e) => setSelectedCityName(e.target.value)}
+        >
+          <option value="">[-Select City-]</option>
+          {cities.data?.map((city) => (
+            <option value={city} key={city}>
+              {city}
+            </option>
+          ))}
         </select>
       </div>
       <div className="w-full h-96 my-10">
-        <LineChart data={weather?.list} />
+        {selectedCityName && (
+          <LineChart
+            data={weather.data?.list}
+            title_text={`${selectedCityName} Weather`}
+            isLoading={weather.isLoading}
+            isError={weather.isError}
+          />
+         )} 
       </div>
     </div>
   );
