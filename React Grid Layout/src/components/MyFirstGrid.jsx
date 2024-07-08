877006bd61f2547402006fd1d27c73c4668139dd
abhishek kamyani <abhishekkumar.bscsf21kdk@iba-suk.edu.pk>
2024-07-08 18:01:27 +0500

    A0

--- React Grid Layout/src/components/MyFirstGrid.jsx
+++ React Grid Layout/src/components/MyFirstGrid.jsx
@@ -9,15 +9,31 @@ export default function MyFirstGrid() {
   ];
   return (
     <GridLayout
-      className="layout"
+      className="layout bg-purple-500"
       layout={layout}
       cols={12}
       rowHeight={30}
       width={1200}
     >
-      <div key="a" className="bg-red-500">a</div>
-      <div key="b" className="bg-green-500">b</div>
-      <div key="c" className="bg-blue-500">c</div>
+      <div key="a" className="bg-red-500">
+        a
+      </div>
+      <div key="b" className="bg-green-500">
+        b
+      </div>
+      <div key="c" className="bg-blue-500">
+        c
+      </div>
+
+      <div key="d" data-grid={{ x: 0, y: 0, w: 1, h: 2, static: true }} className="bg-cyan-300">
+        d
+      </div>
+      <div key="e" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} className="bg-slate-500">
+        e
+      </div>
+      <div key="f" data-grid={{ x: 4, y: 0, w: 1, h: 2 }} className="bg-neutral-400">
+        f
+      </div>
     </GridLayout>
   );
 }
